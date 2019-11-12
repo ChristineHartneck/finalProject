c9712dc2e18e84ca4dc8ea9743850f69c65adc62
Lisa Miller <lisa.miller96@web.de>
2019-11-12 01:14:08 +0100

    modified bootstrap

--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,15 @@
-// $('.dropdown-toggle').dropdown();
+/*
+1.schritt ohne backend: 
+eigene definieren der Cards:
+cards = [...]
+
+2.schritt mit backend: 
+make AJAX call 
+cards = response des AJAXs
+
+dynamisches hinzufügen der cards (ungefähr :D)
+for (0 bis end) {
+    $('#idDesElements').add(<li> {card} </li>);
+}
+*/
 
